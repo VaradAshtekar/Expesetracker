8a65423c062998ea8f864a5f7c6c1c94f52b613e
VaradAshtekar <varadashtekar96@gmail.com>
2020-10-14 19:31:52 +0530

    Updates

---
+++ precache-manifest.ff53b42c2c491e2b90388ac7a7449d10.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39405fd50b1106ac11643cdd0f6ed4c5",
+    "url": "/Expesetracker/index.html"
+  },
+  {
+    "revision": "2f936c61aee9a74686ff",
+    "url": "/Expesetracker/static/css/main.8a50243e.chunk.css"
+  },
+  {
+    "revision": "ee9c2ac9c920b576a833",
+    "url": "/Expesetracker/static/js/2.3fb50242.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Expesetracker/static/js/2.3fb50242.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f936c61aee9a74686ff",
+    "url": "/Expesetracker/static/js/main.26865713.chunk.js"
+  },
+  {
+    "revision": "d13d80b470eff0178434",
+    "url": "/Expesetracker/static/js/runtime-main.1c97086c.js"
+  }
+]);
